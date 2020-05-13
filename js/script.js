9833bb4676915b353512225989cf1611ef2eb2b7
Zhuldyz <example@gmail.com>
2020-05-13 18:11:42 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+// let number = 5;
+// const leftBorderWidth = 1;
+// number = number + 10 +leftBorderWidth;
+// console.log(number);
+// const obj = {
+//  name: "John",
+//  age: 25,
+//  isMarrief: false
+// };
+// console.log(obj.name);
+ 
+// let a = ['p','a','s']
+// console.log(a[1]);
+
+// const result = confirm ("ARE U HERE?")
+// console.log(result);
+//  const answer = +prompt(' How old r u?','18');
+//  console.log(answer)
+
+// const answers = [];
+// // answers[0] = prompt('What is your name?',' my name is ...');
+// // answers[1] = prompt('What is surname?',' my surname is ...');
+// // answers[2] = +prompt('How old r u?','');
+// console.log(typeof(answers));
+let incr = 10, 
+    decr = 10;
+incr++;
+decr--;
+console.log(incr,' ',decr);
